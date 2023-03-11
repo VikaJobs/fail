631179e07a5ef53eb43d0aea624f8b93b0163cb4
VikaJobs <rabotilova@gmail.com>
2023-03-11 12:43:38 +0200

    init

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function () {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(data)), 1000;
+});
+
+const onPlay = function (data) {
+  const array = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+  const currentTime = {
+    duration: 61.857,
+    percent: 0.049,
+    seconds: 3.034,
+  };
+};
+
+player.on('play', onPlay);
